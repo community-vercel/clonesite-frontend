2a84d90ed5865ac6d28c75009f8fa565a3481124
community-vercel <communityvercel@gmail.com>
2025-09-11 00:25:43 +0500

    aaa

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,10 @@ const nextConfig = {
   experimental: {
     turbo: false,
     turbopack: false, // Additional flag for completeness
-        optimizeCss: false, // disables lightningcss, fallback to PostCSS
 
   },
 
 };
+
 
 export default nextConfig;
