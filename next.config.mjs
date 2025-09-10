21a076c690298a550419d4e5e032165174b5fecf
community-vercel <communityvercel@gmail.com>
2025-09-11 00:13:01 +0500

    aaa

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,7 @@ const nextConfig = {
     turbo: false,
     turbopack: false, // Additional flag for completeness
   },
-  // Also try adding this to skip Turbopack entirely
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Custom webpack tweaks if needed, but start empty
-    return config;
-  },
+
 };
 
 export default nextConfig;
